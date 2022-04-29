bccd0dc6e5c47434c823c381835fd0ce967904b7
RahulMR42 <test@example.com>
2022-04-29 17:46:57 +0530

    first draft

--- provider.tf
+++ provider.tf
@@ -1,28 +1,8 @@
 ## Copyright (c) 2022, Oracle and/or its affiliates. 
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
-
-terraform {
-  required_version = ">= 0.14"
-  required_providers {
-    oci = {
-      source = "hashicorp/oci"
-      # version = "4.34.0"
-    }
-   
-  }
-}
-
 provider "oci" {
-  tenancy_ocid = var.tenancy_ocid
-  region       = var.region
-
-
-}
-
-provider "oci" {
-  alias        = "home_region"
-  tenancy_ocid = var.tenancy_ocid
-  region       = lookup(data.oci_identity_regions.home_region.regions[0], "name")
-
+   tenancy_ocid = var.tenancy_ocid
+   alias  = "home_region"
+   region = "${var.region}"
 }
 
